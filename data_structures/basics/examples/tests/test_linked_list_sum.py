713ce196e0892d8e05a7106c2377baca3c9e81e0
Cesar Desales <cesar.desales@magnolia-cms.com>
2013-03-16 23:48:18 +0100

    Test code for sum of linked list

--- data_structures/basics/examples/tests/test_linked_list_sum.py
+++ data_structures/basics/examples/tests/test_linked_list_sum.py
@@ -7,14 +7,14 @@ Created on Mar 13, 2013
 import nose
 from nose.tools import ok_
 from data_structures.basics.linked_list import LinkedList
-from basics.examples.linked_list_sum import LinkedListCalculator
+from data_structures.basics.examples.linked_list_sum import LinkedListCalculator
 
 def build_from_elements(items):
     ll = LinkedList()
     for ii in items:
         ll.insertBeginning(ii)
     return ll
-
+"""
 def int2seq(number):
     return [int(xx) for xx in str(number)]
 
@@ -29,19 +29,20 @@ def test_int2seq():
     ok_(op2 == [2, 9, 5])
         
     ok_( int2seq(513 + 295) == int2seq(808) )
-    
+"""    
 
 def test_sum():
     op1 = (3, 1, 5)
     op2 = (5, 9, 2)
-    expected_result = (8, 0, 8)
+    expected = (8, 0, 8)
     
-    left = build_from_elements(op1)
-    right = build_from_elements(op2)
+    op1_ll = build_from_elements(op1)
+    op2_ll = build_from_elements(op2)
+    expected_ll = build_from_elements(expected)
 
     calc = LinkedListCalculator()
-    result_as_linked_list = calc.sum(left, right)
-    for res, expected in zip(result_as_linked_list.traverse() , build_from_elements(expected_result).traverse()):
+    result = calc.sum(op1_ll, op2_ll)
+    for res, expected in zip(result.traverse() , expected_ll.traverse()):
         ok_(res == expected)
 
 def test_traverse():
@@ -49,9 +50,8 @@ def test_traverse():
     ll = build_from_elements(items)
 
 
-    for res, expected in zip(ll.traverse() , ("AA", "BB", "CC")):
-        ok_(res == expected)
-    print "TR"
+    for res, expected in zip(ll.traverse() , ("CC", "BB", "AA")):
+        ok_(res.cargo == expected)
     
 def run():
     nose.run()
