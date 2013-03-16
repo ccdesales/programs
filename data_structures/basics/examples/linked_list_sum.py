555a02ed628ec7173ec65d7c8f6a33394e208372
Cesar Desales <cesar@cesarcd.com>
2013-03-16 20:39:36 +0100

    1sr draft of test code for sum of linked list

---
+++ data_structures/basics/examples/linked_list_sum.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 13, 2013
+
+@author: desales
+'''
+
+class LinkedListCalculator:
+    def sum(self, lla, llb):
+        pass
+
+if __name__ == '__main__':
+    pass
--- data_structures/basics/examples/tests/test_linked_list_sum.py
+++ data_structures/basics/examples/tests/test_linked_list_sum.py
@@ -7,6 +7,7 @@ Created on Mar 13, 2013
 import nose
 from nose.tools import ok_
 from data_structures.basics.linked_list import LinkedList
+from basics.examples.linked_list_sum import LinkedListCalculator
 
 def build_from_elements(items):
     ll = LinkedList()
@@ -14,10 +15,34 @@ def build_from_elements(items):
         ll.insertBeginning(ii)
     return ll
 
-#def _build_linked_list():
-#    items = ("AA", "BB", "CC", "DD", "EE")
-#    return build_from_elements(items)
+def int2seq(number):
+    return [int(xx) for xx in str(number)]
 
+def test_int2seq():
+    op1 = int2seq(513)
+    op2 = int2seq(295)
+    
+    print op1
+    print op2
+    
+    ok_(op1 == [5, 1, 3])
+    ok_(op2 == [2, 9, 5])
+        
+    ok_( int2seq(513 + 295) == int2seq(808) )
+    
+
+def test_sum():
+    op1 = (3, 1, 5)
+    op2 = (5, 9, 2)
+    expected_result = (8, 0, 8)
+    
+    left = build_from_elements(op1)
+    right = build_from_elements(op2)
+
+    calc = LinkedListCalculator()
+    result_as_linked_list = calc.sum(left, right)
+    for res, expected in zip(result_as_linked_list.traverse() , build_from_elements(expected_result).traverse()):
+        ok_(res == expected)
 
 def test_traverse():
     items = ("AA", "BB", "CC")
@@ -25,8 +50,7 @@ def test_traverse():
 
 
     for res, expected in zip(ll.traverse() , ("AA", "BB", "CC")):
-        print res, expected
-        #ok_(res.cargo == expected, "Bad ordering in list")
+        ok_(res == expected)
     print "TR"
     
 def run():
