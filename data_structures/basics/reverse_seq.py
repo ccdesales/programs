7f11eb378451e2896ee36ded2e97223f1d72ec5d
Cesar Desales <cesar.desales@magnolia-cms.com>
2013-03-31 21:24:05 +0200

    #3 Implement sequence reversal

--- data_structures/basics/reverse_seq.py
+++ data_structures/basics/reverse_seq.py
@@ -4,3 +4,9 @@ Created:
 
 """
 
+def do_reverse(seq):
+    
+    if len(seq):
+        return [seq[-1]] + do_reverse(seq[:-1])
+    else:
+        return type(seq)()
--- data_structures/basics/tests/test_reverse_seq.py
+++ data_structures/basics/tests/test_reverse_seq.py
@@ -1,8 +1,16 @@
 """
-Test set for SetOfStacks
+Test set for reverse_seq
 """
 import nose
 from nose.tools import eq_, ok_
+from data_structures.basics.reverse_seq import do_reverse 
 
 def test_basic():
-    pass
+    eq_( do_reverse([1,2,3]), [3,2,1] )
+    eq_( do_reverse(["A", "B", "C", "D"]), ["D", "C", "B", "A"] )
+
+def run():
+    nose.run()
+    
+if __name__ == '__main__':
+    run()
