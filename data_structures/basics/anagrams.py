dcc6baf45185fbe319ba2278f59cf33406b09d0f
Cesar Desales <cesar.desales@magnolia-cms.com>
2013-04-01 16:05:24 +0200

    #5 Implement anagram detection

--- data_structures/basics/anagrams.py
+++ data_structures/basics/anagrams.py
@@ -4,3 +4,25 @@ Created:
 
 """
 
+def is_anagram(s1, s2):
+    s1 = s1.lower().replace(' ', '')
+    s2 = s2.lower().replace(' ', '')
+    
+    if len(s1) != len(s2):
+        return False
+    
+    counter = dict()
+    for cc in s1:
+        count = counter.get(cc, 0)
+        counter[cc] = count+1
+    
+    for cc in s2:
+        count = counter.pop(cc, 0)
+        if not count:
+            return False
+        
+        count=count-1
+        if count > 0:
+            counter[cc] = count
+
+    return not counter
--- data_structures/basics/tests/test_anagrams.py
+++ data_structures/basics/tests/test_anagrams.py
@@ -3,6 +3,23 @@ Test set for SetOfStacks
 """
 import nose
 from nose.tools import eq_, ok_
+from data_structures.basics.anagrams import is_anagram 
+    
+def test_is_anagram():
+    eq_( is_anagram("Arrigo Boito", "Tobia Gorrio"), True)
+    eq_( is_anagram("Eleven plus two", "Twelve plus one"), True)
+    eq_( is_anagram("I am not active", "Vacation time"), True)
+    eq_( is_anagram("FOOBAR", "foobar"), True)
+    eq_( is_anagram("foobar", "FOOBAR"), True)
 
-def test_basic():
-    pass
+def test_is_not_anagram():
+    eq_( is_anagram("Arrigo Boito", "Tobia GorrioX"), False)
+    eq_( is_anagram("Arrigo BoitoX", "Tobia Gorrio"), False)
+    eq_( is_anagram("Arrigo XBoito", "Tobia Gorrio"), False)
+    eq_( is_anagram("Arrigo Boito", "Tobia XGorrio"), False)
+    
+def run():
+    nose.run()
+    
+if __name__ == '__main__':
+    run()
