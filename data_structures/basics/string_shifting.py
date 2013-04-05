564a70a9dbbd49a820444709969249387045a99e
Cesar Desales <cesar.desales@magnolia-cms.com>
2013-04-05 22:07:15 +0200

    #6 Implement string shift

--- data_structures/basics/string_shifting.py
+++ data_structures/basics/string_shifting.py
@@ -1,6 +1,23 @@
 """
 Author: Cesar C. Desales
-Created: 
+Created: 5 April 2013
 
+How do you shift a String given a String 
+For instance the following string abcdef and given an index 3, how would you make this in to defabc. So basically the index at a given point must be moved to the front and the rest of the string shifted to the right. 
+Another example: Given an index 2 the result is cdefab
 """
 
+def shift(ss, offset):
+    ll=[]
+    
+    ii=offset
+    while ii < len(ss):
+        ll.append(ss[ii])
+        ii += 1
+
+    ii=0
+    while ii < offset:
+        ll.append(ss[ii])
+        ii += 1
+
+    return ''.join(ll)
--- data_structures/basics/tests/test_string_shifting.py
+++ data_structures/basics/tests/test_string_shifting.py
@@ -1,8 +1,22 @@
 """
-Test set for SetOfStacks
+Test set for shift
+Created: 5 April 2013
 """
+
 import nose
 from nose.tools import eq_, ok_
+from data_structures.basics.string_shifting import shift
 
 def test_basic():
-    pass
+    eq_(shift("abc", 1), "bca")
+    eq_(shift("abcd", 1), "bcda")
+    eq_(shift("abcdef", 3), "defabc")
+    eq_(shift("abcdefg", 4), "efgabcd")
+    
+    eq_(len(shift("abc", 1)), len("cab"))
+    
+def run():
+    nose.run()
+    
+if __name__ == '__main__':
+    run()
