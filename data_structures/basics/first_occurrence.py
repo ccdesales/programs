bf12f74575aec63faa008cab3876fb2869b5e06d
Cesar Desales <cesar.desales@magnolia-cms.com>
2013-04-06 10:10:45 +0200

    #2 Implement finding first occurence of string

--- data_structures/basics/first_occurrence.py
+++ data_structures/basics/first_occurrence.py
@@ -4,11 +4,26 @@ Author: Cesar C. Desales
 Goal: Write a program to find the first occurance of one string into another : it is okay to write a O(n2) algorithm
 Created: 30/03/2013
 """
+NOT_FOUND = -1
 
 class StringChecker:
     
     def __init__(self):
         pass
 
-    def first_occurrence(self, string1, string2):
-        return -1
+    def first_occurrence(self, s1, s2):
+        ii=0
+        nn=0
+        if s1 is None or s2 is None:
+            return NOT_FOUND
+        if s1 == '':
+            return 0
+        while ii < len(s1) and nn < len(s2):
+            a, b = s1[ii], s2[nn+ii]
+            if s1[ii] == s2[nn+ii]:
+                return nn + self.first_occurrence(s1[ii+1:], s2[nn+ii+1:])
+            else:
+                nn += 1
+                ii = -1
+            ii += 1
+        return NOT_FOUND
--- data_structures/basics/tests/test_first_occurrence.py
+++ data_structures/basics/tests/test_first_occurrence.py
@@ -3,24 +3,22 @@ Test set for StringChecker
 """
 import nose
 from nose.tools import eq_, ok_
-from data_structures.basics.first_occurrence import StringChecker
+from data_structures.basics.first_occurrence import StringChecker, NOT_FOUND
 
 def test_not_found():
     checker = StringChecker()
-    eq_(checker.first_occurrence("ABCDE", "FGHIJ"), -1)
-    eq_(checker.first_occurrence("ABCDE", "aBCDE"), -1)
-    eq_(checker.first_occurrence("ABCDE", "ABCDe"), -1)
+    
+    eq_(checker.first_occurrence("ABCDE", "aBCDE"), NOT_FOUND)
+    eq_(checker.first_occurrence("ABCDE", "FGHIJ"), NOT_FOUND)
+    eq_(checker.first_occurrence("ABCDE", "ABCDe"), NOT_FOUND)
 
-def test_empty():
+def test_middle():
     checker = StringChecker()
-    eq_(checker.first_occurrence("ABCDE", ""), -1)
-    eq_(checker.first_occurrence("", "ABCDE"), -1)
-    eq_(checker.first_occurrence("", ""), 0)
-
-    eq_(checker.first_occurrence("ABCDE", None), -1)
-    eq_(checker.first_occurrence(None, "ABCDE"), -1)
-    eq_(checker.first_occurrence(None, None), -1)
-
+    eq_(checker.first_occurrence("ABCDE", "xABCDEx"), 1)
+    eq_(checker.first_occurrence("ABCDE", "xyABCDExy"), 2)
+    eq_(checker.first_occurrence("ABCDE", "xyzABCDExyz"), 3)
+    eq_(checker.first_occurrence("ABCDE", "xyzaABCDExyza"), 4)
+    
 def test_find_beginning():
     checker = StringChecker()
     eq_(checker.first_occurrence("ABCDE", "ABCDE"), 0)
@@ -29,8 +27,8 @@ def test_find_beginning():
 def test_find_end():
     checker = StringChecker()
     eq_(checker.first_occurrence("ABCDE", "ABCDE"), 0)
-    eq_(checker.first_occurrence("ABCDE", "xxxxxABCDE"), 0)
-
+    eq_(checker.first_occurrence("ABCDE", "xxxxxABCDE"), 5)
+    
 def test_multi_occurrences():
     checker = StringChecker()
     eq_(checker.first_occurrence("ABCDE", "ABCDExxABCDEyyyABCDE"), 0)
@@ -38,6 +36,23 @@ def test_multi_occurrences():
 
 def test_shorter():
     checker = StringChecker()
-    eq_(checker.first_occurrence("ABCDE", ""), -1)
-    eq_(checker.first_occurrence("ABCDE", "ABCD"), -1)
-    eq_(checker.first_occurrence("ABCDE", "BCDE"), -1)
+    eq_(checker.first_occurrence("ABCDE", ""), NOT_FOUND)
+    eq_(checker.first_occurrence("ABCDE", "ABCD"), NOT_FOUND)
+    eq_(checker.first_occurrence("ABCDE", "BCDE"), NOT_FOUND)
+
+def test_empty():
+    checker = StringChecker()
+    eq_(checker.first_occurrence("ABCDE", ""), NOT_FOUND)
+    eq_(checker.first_occurrence("", "ABCDE"), 0)
+    eq_(checker.first_occurrence("", ""), 0)
+
+    eq_(checker.first_occurrence("ABCDE", None), NOT_FOUND)
+    eq_(checker.first_occurrence(None, "ABCDE"), NOT_FOUND)
+    eq_(checker.first_occurrence(None, None), NOT_FOUND)
+
+def run():
+    nose.run()
+    
+if __name__ == '__main__':
+    run()
+
